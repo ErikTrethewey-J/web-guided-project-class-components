8ad658188399857d8e620422a0be18c0434d98ea
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-09 18:12:51 -0800

    lecture work-in-progress

--- followAlong/src/components/ListForm.js
+++ followAlong/src/components/ListForm.js
@@ -2,6 +2,9 @@ import React from "react";
 
 class ListForm extends React.Component {
   // Constructor with state
+  constructor(props) {
+    super(props);
+  }
 
   handleChanges = e => {
     // update state with each keystroke
