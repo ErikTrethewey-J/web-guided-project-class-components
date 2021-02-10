f951b84d914763844c9bb732b3fb73fc592c158c
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-09 18:27:22 -0800

    lecture work-in-progress

--- followAlong/src/components/ListForm.js
+++ followAlong/src/components/ListForm.js
@@ -17,7 +17,7 @@ class ListForm extends React.Component {
   // class property to submit form
   submitItem = e => {
     e.preventDefault();
-    this.setState({ item: '' });
+    // this.setState({ item: '' });
     this.props.addItem(this.state.item, e);
   }
 
