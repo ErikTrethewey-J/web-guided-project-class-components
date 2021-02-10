9042ab396725283ec23f389f26123352b261cf29
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-09 17:51:55 -0800

    lecture work-in-progress

--- followAlong/src/index.js
+++ followAlong/src/index.js
@@ -59,7 +59,7 @@ class App extends React.Component {
            <h1>Shopping List</h1>
            <ListForm />
          </div>
-        <GroceryList groceries={this.state.groceries} />
+        <GroceryList togglePurchased={this.togglePurchased} groceries={this.state.groceries} />
        </div>
     );
   }
